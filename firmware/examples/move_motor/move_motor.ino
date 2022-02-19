418a48a2f5fa7a7152652b45d6d6d90e2199f737
Oscar Lima <oscar.lima@dfki.de>
2022-02-19 10:49:19 +0100

    code refactor of move_motor example

--- firmware/examples/move_motor/move_motor.ino
+++ firmware/examples/move_motor/move_motor.ino
@@ -1,6 +1,3 @@
-// The I2Cdev, MPU6050 class libraries need to be installed in the Arduino class library folder in advance
-// #include "I2Cdev.h"
-
 #define IN1M 7 // motor1
 #define IN2M 6 // motor 1
 
@@ -12,11 +9,6 @@
 
 #define STBY 8
 
-// Encoder count signal
-#define PinA_left 2  // interrupt 0
-#define PinA_right 4 // interrupt 1
-
-// the setup function runs once when you press reset or power the board
 void setup() {
     // TB6612FNG driver module control signal initialization
     pinMode(IN1M, OUTPUT); // Control the direction of motor 1, 01 is forward rotation, 10 is reverse rotation
@@ -28,6 +20,7 @@ void setup() {
     pinMode(PWMB, OUTPUT); // Right motor PWM
 
     pinMode(STBY, OUTPUT); // enable TB6612FNG
+
     // Initialize the motor driver module
     digitalWrite(IN1M, 0);
     digitalWrite(IN2M, 1);
@@ -36,20 +29,8 @@ void setup() {
     digitalWrite(STBY, 1);
     analogWrite(PWMA, 0);
     analogWrite(PWMB, 0);
-    // Speed ​​dial input
-    pinMode(PinA_left, INPUT);  
-    pinMode(PinA_right, INPUT);
-    // Join the I2C bus
-    // Wire.begin(); // Join the I2C bus sequence
-    // Serial.begin(9600); // Open the serial port and set the baud rate to 115200
-    // delay(1500);
-    // mpu.initialize(); // Initialize MPU6050
-    /// 5ms timer interrupt setting, use timer2. Note: using timer2 will affect the PWM output of pin3 pin11,
-    /// Because PWM uses a timer to control the duty cycle, when using a timer, pay attention to check the pin port of the corresponding timer.
-    // MsTimer2::set(5, inter);
-    // MsTimer2::start();
     
-    delay(300); // 300 ms delay
+    delay(300);
 }
 
 void moveMotor(bool motor, bool direction, int pwm) // set pwm between 0 - 255
@@ -94,10 +75,10 @@ void moveMotor(bool motor, bool direction, int pwm) // set pwm between 0 - 255
 
 // the loop function runs over and over again forever
 void loop() {
-   // moveMotor(true, true, 255);        // move motor 1
-   // moveMotor(false, true, 255);       // move motor 2
-   // delay(1000);                      // wait for a second
-   // moveMotor(true, false, 255);       // move motor 1
-   // moveMotor(false, false, 255);      // move motor 2
-    delay(1000);                      // wait for a second
+    moveMotor(true, true, 255);        // move motor 1
+    moveMotor(false, true, 255);       // move motor 2
+    delay(1000);                       // wait for a second
+    moveMotor(true, false, 255);       // move motor 1
+    moveMotor(false, false, 255);      // move motor 2
+    delay(1000);                       // wait for a second
 }
